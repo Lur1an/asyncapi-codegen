7ffeda73eae6355a5d1b414ed76387e5898c6299
lurian <lurian-code@protonmail.com>
2023-08-19 18:48:05 +0200

    feat(lib.rs): Add trait removal logic and refactor tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 use std::path::PathBuf;
 
-use proc_macro2::{TokenStream, TokenTree};
+use lazy_static::lazy_static;
+use proc_macro2::{Ident, TokenStream, TokenTree};
 use quote::ToTokens;
 use syn::{Attribute, Item, Meta};
 
+/// Joins all source files into one single String file
+/// Adds `use serde::{Deserialize, Serialize};` to the top of the file
+/// Removes all `crate::` from the source files as they will be in one source and can refer to each
+/// other directly
 fn join_inputs(inputs: &[PathBuf]) -> String {
     let mut output = String::new();
     output.push_str("use serde::{Deserialize, Serialize};\n");
@@ -16,6 +21,17 @@ fn join_inputs(inputs: &[PathBuf]) -> String {
     output
 }
 
+fn should_remove_trait(ident: &Ident) -> bool {
+    lazy_static! {
+        static ref BUGGY_TRAITS: [&'static str; 6] =
+            ["Ord", "PartialOrd", "PartialEq", "Eq", "Hash", "Copy"];
+    }
+    BUGGY_TRAITS.contains(&ident.to_string().as_str())
+}
+
+/// Removes buggy traits from the derive macro like Eq, Hash, Copy, Ord, etc.
+/// Will be configurable in the future or will remove all traits and then just insert new ones
+/// depending on config
 fn remove_buggy_traits(
     item: impl IntoIterator<Item = TokenTree>,
 ) -> impl Iterator<Item = TokenTree> {
@@ -26,71 +42,65 @@ fn remove_buggy_traits(
             return false;
         }
         match t {
-            TokenTree::Ident(_) => todo!(),
+            TokenTree::Ident(ident) => {
+                if should_remove_trait(ident) {
+                    skip_next = true;
+                    return false;
+                }
+                true
+            }
             _ => true,
         }
     })
 }
 
 fn edit_derive_traits(attrs: &mut Vec<Attribute>) {
-    log::info!("Editing derive traits");
-    attrs.iter_mut().for_each(|item| match &mut item.meta {
-        Meta::List(meta_list) => {
+    attrs.iter_mut().for_each(|item| {
+        if let Meta::List(meta_list) = &mut item.meta {
             let new_tokens = remove_buggy_traits(meta_list.tokens.clone());
             meta_list.tokens = new_tokens.collect::<TokenStream>();
         }
-        _ => todo!(),
     });
-    log::info!("Finished editing derive traits");
-    log::info!("{:#?}", attrs);
 }
 
 #[cfg(test)]
 mod test {
-    use quote::{ToTokens, __private::Span, quote};
-    use syn::{parse_str, File, Ident, Item};
+    use super::*;
+    use quote::quote;
+    use std::collections::HashMap;
+    use syn::{parse_str, File, Item};
     use test_log::test;
 
-    use super::*;
-
     #[test]
-    fn test_join_inputs() {
-        // let inputs = std::fs::read_dir("./resources/models")
-        //     .unwrap()
-        //     .into_iter()
-        //     .map(Result::unwrap)
-        //     .map(|e| e.path())
-        //     .collect::<Vec<_>>();
-        let inputs = vec![PathBuf::from("./resources/models/anonymous_schema8.rs")];
+    fn dev_test() {
+        let inputs = std::fs::read_dir("./resources/models")
+            .unwrap()
+            .into_iter()
+            .map(Result::unwrap)
+            .map(|e| e.path())
+            .collect::<Vec<_>>();
+        // let inputs = vec![PathBuf::from("./resources/models/anonymous_schema8.rs")];
         let joined_file = join_inputs(&inputs);
         let mut ast = parse_str::<File>(&joined_file).unwrap();
-        println!("{:#?}", ast.items);
+        // println!("{:#?}", ast.items);
+        let mut structs = HashMap::new();
+        let mut enums = HashMap::new();
         for item in ast.items.iter_mut() {
             match item {
-                Item::Const(_) => (),
                 Item::Enum(enum_item) => {
                     edit_derive_traits(&mut enum_item.attrs);
-                    enum_item.ident = Ident::new("DeezNutsOnYourChin", Span::call_site());
+                    enums.insert(enum_item.ident.clone(), enum_item);
                 }
-                Item::ExternCrate(_) => (),
-                Item::Fn(_) => (),
-                Item::ForeignMod(_) => (),
-                Item::Impl(_) => (),
-                Item::Macro(_) => (),
-                Item::Mod(_) => (),
-                Item::Static(_) => (),
-                Item::Struct(_) => (),
-                Item::Trait(_) => (),
-                Item::TraitAlias(_) => (),
-                Item::Type(_) => (),
-                Item::Union(_) => (),
-                Item::Use(_) => (),
-                Item::Verbatim(_) => (),
+                Item::Struct(struct_item) => {
+                    edit_derive_traits(&mut struct_item.attrs);
+                    structs.insert(struct_item.ident.clone(), struct_item);
+                }
                 _ => (),
             }
         }
+        // println!("{:#?}", ast.items);
+        println!("{:?}", structs);
         let new_src = quote! { #ast }.to_string();
-        println!("{:#?}", ast.items);
-        // println!("{}", new_src);
+        println!("{}", new_src);
     }
 }
