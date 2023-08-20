c5b7bbc92c734a3ea4ebbad2aef27e1972b17b72
lurian <lurian-code@protonmail.com>
2023-08-20 16:11:47 +0200

    refactor(lib.rs): Improve code structure and efficiency

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
 use lazy_static::lazy_static;
-use proc_macro2::{Ident, Literal, TokenStream, TokenTree};
-use std::path::PathBuf;
-use syn::{Attribute, Meta};
+use proc_macro2::{Ident, Literal, Span, TokenStream, TokenTree};
+use quote::{quote, ToTokens};
+use std::{collections::HashMap, path::PathBuf};
+use syn::{parse_str, Attribute, Fields, File, Item, Meta};
 
 /// Joins all source files into one single String file
-/// Adds `use serde::{Deserialize, Serialize};` to the top of the file
+/// Adds library imports to the top of the file
 /// Removes all `crate::` from the source files as they will be in one source and can refer to each
 /// other directly
-fn join_inputs(inputs: &[PathBuf]) -> String {
+fn join_inputs(inputs: impl Iterator<Item = PathBuf>) -> String {
     let mut output = String::new();
     output.push_str("use serde::{Deserialize, Serialize};\n");
     output.push_str("use monostate::MustBe;\n");
-    inputs.iter().for_each(|input| {
+    inputs.for_each(|input| {
         let content = std::fs::read_to_string(input).unwrap();
         let content = content.replace("crate::", "");
         output.push_str(&content);
@@ -53,7 +54,7 @@ fn remove_buggy_traits(
     })
 }
 
-fn edit_derive_traits(attrs: &mut Vec<Attribute>) {
+fn edit_derive_traits(attrs: &mut [Attribute]) {
     attrs.iter_mut().for_each(|item| {
         if let Meta::List(meta_list) = &mut item.meta {
             if meta_list.path.segments.first().unwrap().ident != "derive" {
@@ -65,13 +66,19 @@ fn edit_derive_traits(attrs: &mut Vec<Attribute>) {
     });
 }
 
-fn scan_serde_tag(attrs: &mut Vec<Attribute>) -> Option<Literal> {
-    for item in attrs.iter_mut() {
-        if let Meta::List(meta_list) = &mut item.meta {
+fn scan_serde_tag(attrs: &[Attribute]) -> Option<Literal> {
+    for item in attrs.iter() {
+        if let Meta::List(meta_list) = &item.meta {
             if meta_list.path.segments.first().unwrap().ident != "serde" {
                 continue;
             }
-            let tokens = meta_list.tokens.clone().into_iter().collect::<Vec<_>>();
+            let tokens = meta_list
+                .tokens
+                .clone()
+                .into_token_stream()
+                .into_iter()
+                .take(3)
+                .collect::<Vec<_>>();
             match &tokens[..] {
                 [TokenTree::Ident(ident), TokenTree::Punct(punct), TokenTree::Literal(lit)] => {
                     if ident == "tag" && punct.as_char() == '=' {
@@ -79,7 +86,7 @@ fn scan_serde_tag(attrs: &mut Vec<Attribute>) -> Option<Literal> {
                     }
                 }
                 _ => {
-                    return None;
+                    continue;
                 }
             }
         }
@@ -87,53 +94,95 @@ fn scan_serde_tag(attrs: &mut Vec<Attribute>) -> Option<Literal> {
     None
 }
 
-fn replace_single_enum_with_str_const() {}
+/// Removes all field that match the given identifiers
+fn remove_fields_named(fields: &mut Fields, fields_to_remove: &[Ident]) {
+    match fields {
+        Fields::Named(fields) => {
+            fields.named = fields
+                .named
+                .clone()
+                .into_iter()
+                .filter(|f| {
+                    let keep_field = f
+                        .ident
+                        .as_ref()
+                        .map(|identifier| !fields_to_remove.contains(identifier))
+                        .unwrap_or(true);
+                    keep_field
+                })
+                .collect();
+        }
+        _ => panic!("Only named fields are supported"),
+    };
+}
+
+pub fn generate_models(inputs: impl Iterator<Item = PathBuf>) -> String {
+    let joined_file = join_inputs(inputs);
+    let mut ast = parse_str::<File>(&joined_file).unwrap();
+    let mut enums = HashMap::new();
+    // The Literal is the name of the field that needs to be removed in all struct variants
+    // with name in the `Ident` vector
+    let mut duplicate_tags: Vec<(Literal, Vec<Ident>)> = Vec::new();
+    for enum_item in ast.items.iter_mut().filter_map(|i| {
+        if let Item::Enum(enum_item) = i {
+            Some(enum_item)
+        } else {
+            None
+        }
+    }) {
+        edit_derive_traits(&mut enum_item.attrs);
+        // Scanning for a #[serde(tag="value")] is needed because modelina duplicates
+        // the "discriminator" field in all nested structs, however deserialization
+        // will fail this way because the value of the discriminator field <value> will
+        // be consumed by the enum tag resolution and then deserialization of inner
+        // struct fails due to missing <value> field from the left over json fields.
+        if let Some(serde_tag) = scan_serde_tag(&enum_item.attrs) {
+            let variants_to_check = enum_item
+                .variants
+                .iter()
+                .map(|v| v.ident.clone())
+                .collect::<Vec<_>>();
+            duplicate_tags.push((serde_tag, variants_to_check));
+        }
+        enums.insert(enum_item.ident.clone(), enum_item);
+    }
+
+    for struct_item in ast.items.iter_mut().filter_map(|i| {
+        if let Item::Struct(struct_item) = i {
+            Some(struct_item)
+        } else {
+            None
+        }
+    }) {
+        edit_derive_traits(&mut struct_item.attrs);
+        if let Some((field_name, _)) = duplicate_tags
+            .iter()
+            .find(|(_, variants)| variants.contains(&struct_item.ident))
+        {
+            remove_fields_named(
+                &mut struct_item.fields,
+                &[Ident::new(
+                    &field_name.to_string().replace('"', ""),
+                    Span::call_site(),
+                )],
+            );
+        }
+    }
+    quote! { #ast }.to_string()
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use quote::quote;
-    use std::collections::HashMap;
-    use syn::{parse_str, File, Item};
     use test_log::test;
 
     #[test]
-    fn dev_test() {
-        // let inputs = std::fs::read_dir("./resources/models")
-        //     .unwrap()
-        //     .into_iter()
-        //     .map(Result::unwrap)
-        //     .map(|e| e.path())
-        //     .collect::<Vec<_>>();
-        let inputs = vec![PathBuf::from(
-            "./resources/models/one_api_request_payload.rs",
-        )];
-        let joined_file = join_inputs(&inputs);
-        let mut ast = parse_str::<File>(&joined_file).unwrap();
-        println!("{:#?}", ast.items);
-        let mut structs = HashMap::new();
-        let mut enums = HashMap::new();
-        for item in ast.items.iter_mut() {
-            match item {
-                Item::Enum(enum_item) => {
-                    edit_derive_traits(&mut enum_item.attrs);
-                    // Scanning for a #[serde(tag="value")] is needed because modelina duplicates
-                    // the "discriminator" field in all nested structs, however deserialization
-                    // will fail this way because the value of the discriminator field <value> will
-                    // be consumed by the enum tag resolution and then deserialization of inner
-                    // struct fails due to missing <value> field from the left over json fields.
-                    let serde_tag = scan_serde_tag(&mut enum_item.attrs);
-                    enums.insert(enum_item.ident.clone(), enum_item);
-                }
-                Item::Struct(struct_item) => {
-                    edit_derive_traits(&mut struct_item.attrs);
-                    structs.insert(struct_item.ident.clone(), struct_item);
-                }
-                _ => (),
-            }
-        }
-        // println!("{:#?}", ast.items);
-        let new_src = quote! { #ast }.to_string();
-        // println!("{}", new_src);
+    fn test_generate_models() {
+        let inputs = std::fs::read_dir("./resources/models")
+            .unwrap()
+            .into_iter()
+            .map(Result::unwrap)
+            .map(|e| e.path());
+        let _ = generate_models(inputs);
     }
 }
